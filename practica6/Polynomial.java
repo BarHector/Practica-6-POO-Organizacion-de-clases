c7cb6fa4d19ea3f2e85d190cf01ed27a1f66158c
Barriguete Hector <hectorrodjls@gmail.com>
2021-10-12 17:58:41 -0500

    Version 1.1.0 Se agregó la documentación de la clase Polynomial.

--- practica6/Polynomial.java
+++ practica6/Polynomial.java
@@ -2,7 +2,16 @@ package practica6;
 
 import java.util.ArrayList;
 
+/**
+* Esta clase crea un polinomio apartir de un String el cual lo guarda en un String, además deja realizar suma y resta de polinomios, así como la multiplicacion
+* por un escalar.
+* @author: Barriguete Rodríguez Héctor Alejandro
+* @version: 12/10/2021
+*/
+
 public class Polynomial {
+
+    //Campos de la clase
     private int degree;
     private int value;
     private ArrayList<Double> coeff;
@@ -11,16 +20,27 @@ public class Polynomial {
     private ArrayList<Double> resultM;
     private String vals;
     
+    /**
+    * Constructor vacio de la clase 
+    */
     public Polynomial(){
         degree = 0;
         vals="";
     }
 
+    /**
+    * Constructor con parametros de la clase 
+    * @param degree define el grado de un polinomio
+    * @param vals contienes todos los coeficientes del polinomio separados por un espacio
+    */
     public Polynomial(int degree, String vals){
         this.degree = degree;
         this.vals = vals;
     }
 
+    /**
+    * Creacion de las ArrayList a partir de un String
+    */
     public void createList(){
         coeff = new ArrayList<>(); //INICIALIZANDO EL ARRAYLIST
 
@@ -43,6 +63,11 @@ public class Polynomial {
         }
     }
 
+    /**
+    * Suma de dos ArrayList apartir de sus objetos
+    * @param lista1 es el objeto que contiene un polinomio
+    * @param lista2 es el objeto que contiene otro polinomio(es un objeto distinto al anterior)
+    */
     public void add(Polynomial lista1, Polynomial lista2){
         double suma=0.0;
 
@@ -64,6 +89,11 @@ public class Polynomial {
         }
     }
 
+    /**
+    * Resta de dos ArrayList apartir de sus objetos
+    * @param lista1 es el objeto que contiene un polinomio
+    * @param lista2 es el objeto que contiene otro polinomio(es un objeto distinto al anterior)
+    */
     public void substract(Polynomial lista1, Polynomial lista2){
         double resta=0.0;
 
@@ -85,6 +115,11 @@ public class Polynomial {
         }
     }
 
+    /**
+    * Multiplicacion de la lista de un objeto con un escalar
+    * @param lista1 es el objeto que contiene un polinomio
+    * @param escalar es el valor por el cual se van a multiplicar todos los coeficientes del polinomio
+    */
     public void multiplication(Polynomial po, int escalar){
         resultM = new ArrayList<>();
         
@@ -95,6 +130,10 @@ public class Polynomial {
         printP(resultM);
     }
 
+    /**
+    * Impresion del polinomio mostrando 'x' y potencia
+    * @param polinomioR es la lista que contiene todos los coeficientes de un polinomio
+    */
     public void printP(ArrayList<Double> polinomioR){
         String finalR="";
         int converI=0;
@@ -134,6 +173,11 @@ public class Polynomial {
         System.out.println(finalR);
     }
 
+    /**
+    * Evaluacion de los ArrayList del medotos 'add' y 'substract' apartir de un valor para X
+    *@param polinomioE es la lista con los coeficientes de un polinomio
+    *@param valX es el valor por el cual se va a evaluar el polinomio mandado anteriormente
+    */
     public void evaluacionAS(ArrayList<Double> polinomioE, int valX){
         double resultado=0.0;
         int converI=0;
@@ -154,30 +198,58 @@ public class Polynomial {
         }
     }
 
+    /**
+    * Metodo que devuelve el grado de un polinomio 
+    * @return grado del polinomio
+    */
     public int getDegree(){
         return degree;
-    }
+    } 
 
+    /**
+    * Metodo que devuelve el valor con el que se va a evaluar un polinomio 
+    * @return valor con el que se va a evaluar el polinomio
+    */
     public int getValue(){
         return value;
     }
-
+    
+    /**
+    * Metodo que devuelve la lsita de coeficientes resultantes de la resta de dos polinomios 
+    * @return Lista de coeficientes resultantes de la suma de polinomios
+    */
     public ArrayList<Double> getResultA(){
         return resultA;
     }
-
+    
+    /**
+    * Metodo que devuelve la lista de coeficientes resultantes de la resta de dos polinomios 
+    * @return Lista de coeficientes resultantes de la resta de polinomios
+    */
     public ArrayList<Double> getResultS(){
         return resultS;
     }
-
+    
+    /**
+    * Metodo que devuelve la lista de coeficientes de un polinomio 
+    * @return Lista de coeficientes de un polinomio
+    */
     public ArrayList<Double> getCoeff(){
         return coeff;
     }
 
+    /**
+    * Envio del grado para un polinomio
+    @param degree grado del polinomio
+    */
     public void setDegree(int degree){
         this.degree = degree;
     }
 
+    /**
+    * Envio del valor con el que se va a evaluar un polinomio
+    * @param value valor por el que se va a evaluar a un polinomio
+    */
     public void setValue(int value){
         this.value = value;
     }
